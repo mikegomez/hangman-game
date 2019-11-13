5769fcbcc02c890f50d467d7aad51068bbc2cc27
Mike Gomez <mikegomez21@hotmail.com>
2019-11-12 22:28:37 -0600

    refactor site

---
+++ hang.js
@@ -0,0 +1,36 @@
+ 
+    var words = ['bird', 'cow'];
+    
+    var word = words[Math.floor(Math.random ())];        
+    
+    var answerArray = [];
+        for (i = 0; i< word.length; i++) {
+            answerArray[i] = "_";
+        }
+    
+    // var remainingLetters = word.length;
+    // //game loop
+    //     while (remainingLetters > 0) {
+    //         return(answerArray.join(" "));
+    
+    //         var guess = prompt("Guess a letter");
+    //         if (guess == null) {
+    //             break;
+    //         } else if (guess.length !==1) {
+    //                 return("Pick a letter");
+    //         }  else {
+    //             //udpate game status
+    //             for (var i =0; i< word.length; i++){
+    //                 if (word[i] === guess) {
+    //                     answerArray[i] = guess;
+    //                     remainingLetters--;
+    //                     }
+    //                 }
+    //                 }
+    //             }
+            
+     
+    // alert(answerArray.join(" "));
+    // alert("Good job! The answer is " + word);
+        
+        
